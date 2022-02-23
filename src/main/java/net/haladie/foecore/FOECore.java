002f6aef943bc93b774cf77621be84739e285d05
fyre-infinite <kraykeninfinite@gmail.com>
2022-02-22 23:06:54 -0600

    Updating for Mythril
    I'm testing if I can add items in properly.

--- src/main/java/net/haladie/foecore/FOECore.java
+++ src/main/java/net/haladie/foecore/FOECore.java
@@ -1,21 +1,17 @@
 package net.haladie.foecore;
 
 import net.fabricmc.api.ModInitializer;
+import net.haladie.foecore.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FOECore implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+public class FOECore implements ModInitializer
+{
 	public static final Logger LOGGER = LoggerFactory.getLogger("foecore");
-
+	public static final String MOD_ID = "foecore";
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+	public void onInitialize()
+	{
+		ModItems.registerModItems();
 	}
 }
