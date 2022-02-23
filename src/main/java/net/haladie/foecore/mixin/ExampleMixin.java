1ad33d996cc884e4040b5ea73b26104a3c853acc
fyre-infinite <kraykeninfinite@gmail.com>
2022-02-22 21:31:46 -0600

    Initial Push

---
+++ src/main/java/net/haladie/foecore/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.haladie.foecore.mixin;
+
+import net.haladie.foecore.FOECore;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		FOECore.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
